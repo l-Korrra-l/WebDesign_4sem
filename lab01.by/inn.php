a5495a69a6f0553fb050a36a1998cf4a6bcaa1b7
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2021-05-19 00:10:57 +0300

    -

---
+++ lab01.by/inn.php
@@ -0,0 +1,6 @@
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+    //    date_default_timezone_set('UTC');
+        echo date('l jS \of F Y h:i:s A');
+       // echo json_encode(array('success' => 1)); 
+?>
